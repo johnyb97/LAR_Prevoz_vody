709edb73f3434f5709f5957f5edf8abbf0505532
Jirbu <50017283+Jirbu@users.noreply.github.com>
2019-05-03 16:19:54 +0200

    eclipse

--- Debug/Src/Bluetooth.d
+++ Debug/Src/Bluetooth.d
@@ -1,91 +1,91 @@
 Src/Bluetooth.o: ../Src/Bluetooth.c \
- C:/Users/janbe/Desktop/Pokus/Pokus/Inc/bluetooth.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Inc/main.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Inc/stm32l0xx_hal_conf.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/core_cm0plus.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/core_cmInstr.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/core_cmFunc.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
- C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc/bluetooth.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc/main.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc/stm32l0xx_hal_conf.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
+ C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Inc/bluetooth.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc/bluetooth.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Inc/main.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc/main.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Inc/stm32l0xx_hal_conf.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc/stm32l0xx_hal_conf.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/core_cm0plus.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/core_cm0plus.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/core_cmInstr.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include/core_cmFunc.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_tim_ex.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
 
-C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
+C:/Users/jirka/Documents/skola/4\ semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
