9ec8a79474943714a7d50ac2ab85536b1ec455ea
Jirbu <50017283+Jirbu@users.noreply.github.com>
2019-05-03 16:07:41 +0200

    eclise

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -5,6 +5,7 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Src/Bluetooth.c \
+../Src/Cerpadlo.c \
 ../Src/External_interupt.c \
 ../Src/Step_eng.c \
 ../Src/Ultrasound.c \
@@ -16,6 +17,7 @@ C_SRCS += \
 
 OBJS += \
 ./Src/Bluetooth.o \
+./Src/Cerpadlo.o \
 ./Src/External_interupt.o \
 ./Src/Step_eng.o \
 ./Src/Ultrasound.o \
@@ -27,6 +29,7 @@ OBJS += \
 
 C_DEPS += \
 ./Src/Bluetooth.d \
+./Src/Cerpadlo.d \
 ./Src/External_interupt.d \
 ./Src/Step_eng.d \
 ./Src/Ultrasound.d \
@@ -42,7 +45,7 @@ Src/%.o: ../Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32L073xx -I"C:/Users/janbe/Desktop/Pokus/Pokus/Inc" -I"C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc" -I"C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy" -I"C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Device/ST/STM32L0xx/Include" -I"C:/Users/janbe/Desktop/Pokus/Pokus/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32L073xx -I"C:/Users/jirka/Documents/skola/4 semestr/LAR-hLAVNI/LAR_Prevoz_vody/Inc" -I"C:/Users/jirka/Documents/skola/4 semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc" -I"C:/Users/jirka/Documents/skola/4 semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy" -I"C:/Users/jirka/Documents/skola/4 semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Device/ST/STM32L0xx/Include" -I"C:/Users/jirka/Documents/skola/4 semestr/LAR-hLAVNI/LAR_Prevoz_vody/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
